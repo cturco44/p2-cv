0b1bb9bae410c32dbb1f418771b57957801c9822
cturco <cturco@umich.edu>
2020-02-04 20:47:52 -0500

    finished compute vertical cost matrix

--- processing.cpp
+++ processing.cpp
@@ -117,6 +117,43 @@ void compute_energy_matrix(const Image* img, Matrix* energy) {
 //           computed and written into it.
 //           See the project spec for details on computing the cost matrix.
 void compute_vertical_cost_matrix(const Matrix* energy, Matrix *cost) {
+    int width = Matrix_width(energy);
+    int height = Matrix_height(energy);
+    
+    Matrix_init(cost, width, height);
+    
+    
+    const int *end = Matrix_at(cost, 0, width - 1);
+    
+    //Fills first row of cost with energy values
+    for (const int *start = Matrix_at(cost, 0, 0); start < end; ++start) {
+        
+        int column = Matrix_column(cost, start);
+        *Matrix_at(cost, 0, column) = *Matrix_at(energy, 0, column);
+    }
+    
+    for (int row = 1; row < height - 1; ++row) {
+        for (int col = 0; col < width - 1; ++col) {
+            
+            int c_before = col - 1;
+            int c_next = col + 1;
+            //Makes sure that don't look out of bounds of matrix
+            if(c_before < 0) {
+                c_before = 0;
+            }
+            if(c_next > width - 1) {
+                c_next = width - 1;
+            }
+            
+            int min = Matrix_min_value_in_row(cost, row - 1, c_before, c_next);
+            int cost_value = *Matrix_at(energy, row, col) + min;
+            
+            *Matrix_at(cost, row, col) = cost_value;
+            
+        }
+    }
+    
+    
   assert(false); // TODO Replace with your implementation!
 }
 
